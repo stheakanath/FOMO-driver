3a2e6f5eb2c8fdd4cdb8639055852200a33d7e2f
Tony Jiang <tonyjiang9398@gmail.com>
2016-04-15 02:23:09 -0400

    delete testing

--- testing/test.py
+++
@@ -1,9 +0,0 @@
-import soundcloud
-import piq_utils
-import config
-
-client = soundcloud.Client(client_id=config.client_id)
-
-tracks = client.get('/tracks', q='Darren Styles & Re-Con Feat Matthew Steeper - Rest Of Your Life (Robokid Hypermix)', limit=10)
-
-test = piq_utils.get_all_comments(client, tracks[0])
